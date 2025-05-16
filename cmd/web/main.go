b8f9cf97739faec83936bb8b2ab0bae72baf56e5
newspaperhinson <newspaperhinson@gmail.com>
2025-05-16 23:28:12 +0800

    feat: initialize basic web server with hello world endpoint

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import "github.com/newspaperhinson/e-commerce-go/internal/web"
 
 func main() {
-	fmt.Println("Hello World")
+	app := web.NewApp()
+
+	app.Listen(8080)
 }
---
+++ internal/web/app.go
@@ -0,0 +1,28 @@
+package web
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type App struct{}
+
+func NewApp() *App {
+	app := App{}
+	app.Bootstrap()
+
+	return &app
+}
+
+func (a *App) Bootstrap() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+}
+
+func (a *App) Listen(port int) {
+	fmt.Println(fmt.Sprintf("Listening on port %d...", port))
+
+	http.ListenAndServe(":"+strconv.Itoa(port), nil)
+}
