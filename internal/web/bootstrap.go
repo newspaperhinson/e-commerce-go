445543c7cc2fd2327537d6415488068f803cf71e
newspaperhinson <newspaperhinson@gmail.com>
2025-05-17 00:58:30 +0800

    feat: add web server bootstrap with basic HTTP handler and port configuration

--- internal/web/bootstrap.go
+++ internal/web/bootstrap.go
@@ -6,19 +6,15 @@ import (
 	"strconv"
 )
 
-type App struct{}
-
 func Bootstrap() {
-	app := &App{}
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
 	})
 
-	serve(app, 8080)
+	serve(8080)
 }
 
-func serve(app *App, port int) {
+func serve(port int) {
 	fmt.Printf("Listening on port %d...\n", port)
 
 	http.ListenAndServe(":"+strconv.Itoa(port), nil)
