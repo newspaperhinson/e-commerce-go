fbfb3b63d8234658cfb18dd7741f4d2fa7c9f457
newspaperhinson <newspaperhinson@gmail.com>
2025-05-18 04:53:12 +0800

    feat: create initial web app structure with bootstrap and listener setup

---
+++ apps/web/cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func bootstrap() {
+	fmt.Println("Bootstraping Web App")
+}
+
+func main() {
+	bootstrap()
+}
---
+++ apps/web/web_app.go
@@ -0,0 +1,11 @@
+package web
+
+type App struct {
+}
+
+func NewApp() *App {
+	return &App{}
+}
+
+func (a *App) Listen(address string) {
+}
